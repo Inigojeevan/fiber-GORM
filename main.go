71534a77e8bd1522bb9994c6b3baf2a5fe5f8d29
Inigojeevan <inmasm123@gmail.com>
2023-12-15 14:23:11 +0530

    user:routes

--- main.go
+++ main.go
@@ -16,6 +16,9 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/api", welcome)
 	app.Post("/api/users", routes.CreateUser)
 	app.Get("/api/users", routes.GetAllUsers)
+	app.Get("/api/users/:id", routes.GetUser)
+	app.Put("/api/users/:id", routes.UpdateUser)
+	app.Delete("/api/users/:id", routes.DeleteUser)
 }
 
 func main() {
--- routes/user.go
+++ routes/user.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"errors"
+
 	"github.com/Inigojeevan/fiber-GORM/database"
 	"github.com/Inigojeevan/fiber-GORM/models"
 	"github.com/gofiber/fiber/v2"
@@ -38,4 +40,74 @@ func GetAllUsers(c *fiber.Ctx) error {
 		responseUsers = append(responseUsers, responseUser)
 	}
 	return c.Status(200).JSON(responseUsers)
+}
+
+func helperFunc(id int, user *models.User) error {
+	database.Database.Db.Find(&user, "id = ?", id)
+	if user.ID == 0 {
+		return errors.New("user doesnt exists")
+	}
+	return nil
+}
+func GetUser(c *fiber.Ctx) error {
+	var user models.User
+	id, err := c.ParamsInt("id") //if nothing is found, it returns 0
+
+	if err != nil {
+		return c.Status(400).JSON("Ensure to check the id as int")
+	}
+	if err := helperFunc(id, &user); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+	responseUser := CreateResponseUser(user)
+
+	return c.Status(200).JSON(responseUser)
+}
+
+func UpdateUser(c *fiber.Ctx) error {
+	var user models.User
+
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return c.Status(400).JSON("Ensure to check the id as int")
+	}
+	if err := helperFunc(id, &user); err != nil {
+		return c.Status(400).JSON(err.Error)
+	}
+
+	type UpdateUsers struct {
+		FirstName string `json:"first_name"`
+		LastName  string `json:"last_name"`
+	}
+
+	var updateusers UpdateUsers
+
+	if err := c.BodyParser(&updateusers); err != nil {
+		return c.Status(500).JSON(err.Error())
+	}
+
+	user.FirstName = updateusers.FirstName
+	user.LastName = updateusers.LastName
+
+	database.Database.Db.Save(&user)
+	responseUser := CreateResponseUser(user)
+	return c.Status(400).JSON(responseUser)
+}
+
+func DeleteUser(c *fiber.Ctx) error {
+	var user models.User
+
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return c.Status(400).JSON("Ensure to check the id as int")
+	}
+	if err := helperFunc(id, &user); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	if err := database.Database.Db.Delete(&user).Error; err != nil {
+		return c.Status(404).JSON(err.Error())
+	}
+
+	return c.Status(400).SendString("Successfully deleted the user")
 }
